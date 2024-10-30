b5a93d2bebfe7cc5deff253d0557de2badb7be8d
Avishek Chandra <achandra@costar.com>
2024-10-30 15:27:18 -0700

    Added IsAuth label on top of checkboxes

--- web/static/scripts/headers-toggle.js
+++ web/static/scripts/headers-toggle.js
@@ -2,13 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleHeadersBtn = document.getElementById('toggleHeadersBtn');
     const toggleIcon = document.getElementById('toggleIcon');
     const headersSection = document.getElementById('headersGrid');
+    const isAuthLbl = document.getElementById('isAuthLbl')
 
     toggleHeadersBtn.addEventListener('click', function () {
         if (headersSection.style.display === 'none') {
+            isAuthLbl.style.display = 'grid';
             headersSection.style.display = 'grid';
             toggleIcon.src = toggleIcon.getAttribute('data-eye-slash'); // Show eye-slash icon
         } else {
             headersSection.style.display = 'none';
+            isAuthLbl.style.display = 'none';
             toggleIcon.src = toggleIcon.getAttribute('data-eye'); // Show eye icon
         }
     });
